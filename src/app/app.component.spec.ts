be9b76e22ec1f8af6ad0a6ce281c6e47261bbd3b
palak <palak8856@gmail.com>
2024-11-29 13:04:44 +0530

    Product App

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'product_management_app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('product_management_app');
+    expect(app.name).toEqual('product_management_app');
   });
 
   it('should render title', () => {
